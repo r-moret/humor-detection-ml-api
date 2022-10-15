3715923996a64efa1b35c9fd1625a2875453f205
r-moret <rafaelmoretgalan@gmail.com>
2022-10-16 01:42:21 +0200

    🎨 CSS colors adjustment

--- frontend/utils/css.py
+++ frontend/utils/css.py
@@ -1,11 +1,22 @@
 import streamlit as st
+from .config import load_config
 
 
 def load_css():
+    config = load_config()
+    colors = ["primaryColor", "backgroundColor", "secondaryBackgroundColor", "textColor"]
+
     with open("styles/principal.css") as f:
         st.markdown(
             f"""
                 <style>
+                    :root {{
+                        --primaryColor: {config["theme"]["primaryColor"]};
+                        --backgroundColor: {config["theme"]["backgroundColor"]};
+                        --secondaryBackgroundColor: {config["theme"]["secondaryBackgroundColor"]};
+                        --textColor: {config["theme"]["textColor"]};
+                        --accentTextColor: {config["theme"]["accentTextColor"]};                        
+                    }}
                     {f.read()}
                 </style>
             """, 
